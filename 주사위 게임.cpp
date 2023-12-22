42d1c8325de88aab7cb722b5ed018af544dbd5d4
DESKTOP-H7K9H4B\user <dbsgh725@naver.com>
2023-12-22 18:46:30 +0900

    초기 커밋.

---
+++ 주사위 게임.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<ctime>
+//#include<cstdlib>
+//using namespace std;
+//
+//int main()
+//{
+//	srand(time(NULL));
+//	int dice[6];
+//	int sum=0;
+//	for (int i = 0; i < 6; i++)
+//	{
+//		dice[i] = rand() % 6 + 1;
+//		cout <<i+1<<"번 주사위 :"<< dice[i] << endl;
+//		sum += dice[i];
+//	}
+//	cout << "주사위의 합 : " << sum;
+//	
+//}
---
+++ 화씨온도를 섭씨온도로 바꾸기.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	while (1) {
+//	double h, s;
+//	int mode;
+//	cout << "È­¾¾ -> ¼·¾¾´Â 0, ¼·¾¾ -> È­¾¾´Â 1À» ÀÔ·Â :  ";
+//	cin>>mode;
+//	
+//		switch (mode)
+//		{
+//		case 0:
+//			cout << "È­¾¾¿Âµµ¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ";
+//			cin >> h;
+//			s = (5.0 / 9.0) * (h - 32);
+//			cout << "¼·¾¾ ¿Âµµ´Â : " << s<<endl;
+//			break;
+//		case 1:
+//			cout << "¼·¾¾¿Âµµ¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ";
+//			cin >> s;
+//			h = s / (5.0 / 9.0) + 32;
+//			cout << "È­¾¾ ¿Âµµ´Â : " << h << endl;
+//			break;
+//		default:
+//			cout << "Àß¸ø ÀÔ·Â ÇÏ¼Ì½À´Ï´Ù.";
+//			break;
+//		}
+//
+//	}
+//}
